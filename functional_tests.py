09110a03b2b92161527dc8b851e60071d3f08479
Uwe Rosner <oscottmusic@posteo.de>
2018-06-16 19:53:45 +0200

    CHN Test benutzt jetzt das Python-Unittest-Modul.

--- functional_tests.py
+++ functional_tests.py
@@ -4,35 +4,42 @@
 # Erstellt wird eine TODO-Listen-WebApp 
 
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    
+    def tearDown(self):
+        self.browser.quit()
 
-# Edith hat von einer neuen, coolen online App gehört, die TODO-Listen 
-# verwalten kann. Sie geht gleich mal auf die Homepage..
-browser.get('http://localhost:8000')
+    def test_can_start_a_list_and_retrive_it_later(self):
+        # Edith hat von einer neuen, coolen online App gehört, die TODO-Listen 
+        # verwalten kann. Sie geht gleich mal auf die Homepage..
+        self.browser.get('http://localhost:8000')
 
-# Sie bemerkt, dass der Seiten-Titel den Text "To-Do" beinhaltet
-assert 'To-Do' in browser.title
+        # Sie bemerkt, dass der Seiten-Titel den Text "To-Do" beinhaltet
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test.')
 
-# In eine Textbox kann sie einen ersten TODO-Eintrag eingeben
+        # In eine Textbox kann sie einen ersten TODO-Eintrag eingeben
 
-# Sie schreibt "Kaufe blaue Pfauenfedern" in eine Textbox
+        # Sie schreibt "Kaufe blaue Pfauenfedern" in eine Textbox
 
-# Sobald sie ENTER drückt, wird die Seite aktualisiert und zeigt nun an
-# "1: Kaufe blaue Pfauenfedern" als ein Eintrag in einer TODO-Liste
+        # Sobald sie ENTER drückt, wird die Seite aktualisiert und zeigt nun an
+        # "1: Kaufe blaue Pfauenfedern" als ein Eintrag in einer TODO-Liste
 
-# Die Textbox zur Eingabe von TODO-Einträgen ist immer noch da. Sie 
-# gibt nun ein: "Stelle die Pfauenfedern in eine Vase"
+        # Die Textbox zur Eingabe von TODO-Einträgen ist immer noch da. Sie 
+        # gibt nun ein: "Stelle die Pfauenfedern in eine Vase"
 
-# Die Seite aktualisiert sich erneut und zeigt nun beide Einträge in 
-# der TODO-Liste an
+        # Die Seite aktualisiert sich erneut und zeigt nun beide Einträge in 
+        # der TODO-Liste an
 
-# Edith fragt sich, ob die Seite sich wohl ihre Liste merken kann. 
-# Dann bemerkt sie, dass die Seite eine individuelle URL für sie 
-# erstellt hat -- Dazu gibt es auch einen erklärenden Text
+        # Edith fragt sich, ob die Seite sich  ihre Liste merken kann. 
+        # Dann bemerkt sie, dass die Seite eine individuelle URL für sie 
+        # erstellt hat -- Dazu gibt es auch einen erklärenden Text
 
-# Sie ruft die individuelle URL auf - Ihre TODO-Liste ist noch da.
+        # Sie ruft die individuelle URL auf - Ihre TODO-Liste ist noch da.
 
-# Zufriden geht sie ins Bett und schläfte ein.
-
-browser.quit();
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
